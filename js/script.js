4cee3b164d9e2f66942ad7b4379eba79a55b7008
Jomar Rumbawa <rumbawajomar@gmail.com>
2023-01-17 23:48:08 -0800

    Add eventListener to squares

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const sketchpad = document.getElementById('sketchpad')
 
 for (let i = 0; i < 256; i++) {
   const square = document.createElement('div')
+  square.addEventListener('mouseover', function () {
+    square.classList.add('hovered')
+  })
   square.style.width = '37.5px'
   square.style.height = '37.5px'
   sketchpad.appendChild(square)
